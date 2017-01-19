55dec4cf33ed29036d5b7ae5dbed2179d74adac9
waders92 <wade.carlson@pga.com>
2017-01-19 11:46:27 -0600

    updated user fields and ran migrations

---
+++ db/migrate/20170119174316_add_index_to_rounds.rb
@@ -0,0 +1,6 @@
+class AddIndexToRounds < ActiveRecord::Migration
+  def change
+  end
+
+  add_index :rounds, :user_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119163143) do
+ActiveRecord::Schema.define(version: 20170119174316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20170119163143) do
     t.integer  "putts"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "rounds", ["user_id"], name: "index_rounds_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -41,6 +44,8 @@ ActiveRecord::Schema.define(version: 20170119163143) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
