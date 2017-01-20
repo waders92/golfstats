eda13d4d2a17f55b9b483d3111a952e78e8bb15a
waders92 <wade.carlson@pga.com>
2017-01-20 15:49:39 -0600

    message model and controller added

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119231118) do
+ActiveRecord::Schema.define(version: 20170120214745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: true do |t|
+    t.string   "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "rounds", force: true do |t|
     t.string   "course"
