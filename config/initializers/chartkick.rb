2376763e4ced9b96615cbac9a534d12d19358a9e
waders92 <wade.carlson@pga.com>
2017-01-22 22:32:38 -0600

    styling chart

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,4 +1,4 @@
 Chartkick.options = {
   height: "200px",
-  colors: ["red", "yellow", "blue", "green", "grey", "orange", "lightblue"]
+  colors: ["red", "black", "blue", "green", "grey", "orange", "lightblue"]
 }
