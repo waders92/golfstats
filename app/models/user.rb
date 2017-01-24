29a937f6ed93ebdd124e3eb87ba73bf0a07c782a
waders92 <wade.carlson@pga.com>
2017-01-24 16:17:01 -0600

    can't see messages on main page unless logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  attr_accessor :course, :score
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
