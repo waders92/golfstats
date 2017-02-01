9ac81284832b97025d1f31530e0a5889ac5dc660
waders92 <wade.carlson@pga.com>
2017-02-01 14:12:08 -0600

    ranks indexed correctly

--- app/models/scoring_average.rb
+++ app/models/scoring_average.rb
@@ -1,18 +1,18 @@
 module ScoringAverage
 
+  attr_reader :ranking
+
   def scoring_average
     rounds.average(:score)
   end
-
 
   def scoring_average_rounded
     scoring_average.round(2)
   end
 
   def best_scoring_average
-      @users.each do |user|
-      user.scoring_average_rounded.sort.pluck
-      end
+
   end
+
 
 end
