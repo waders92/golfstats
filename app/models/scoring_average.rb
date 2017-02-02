199c20b58ebdad10784c44384632b6331a765db3
waders92 <wade.carlson@pga.com>
2017-02-02 17:31:50 -0600

    back to old

--- app/models/scoring_average.rb
+++ app/models/scoring_average.rb
@@ -8,4 +8,13 @@ module ScoringAverage
     scoring_average.round(2)
   end
 
+  # def best_user_scoring_average
+  #   x = []
+  #   user.scoring_average_rounded.each do |score|
+  #     x << score
+  #   end
+  #   x.sort
+  #   x.pluck
+  # end
+
 end
