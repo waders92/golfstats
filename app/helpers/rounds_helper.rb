e3138ec84d6e7b832449f9d2c27f3bfea7d3b4f3
waders92 <wade.carlson@pga.com>
2017-02-07 15:02:54 -0600

    refactored ranking views, moved logic to helper methods

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -1,2 +1,27 @@
 module RoundsHelper
+
+  def scoring_rank
+    Round.group(:user).average(:score)
+  end
+
+  def greens_rank
+    Round.group(:user).average(:greens)
+  end
+
+  def fairways_rank
+    Round.group(:user).average(:fwys)
+  end
+
+  def putting_rank
+    Round.group(:user).average(:putts)
+  end
+
+  def birdies_rank
+    Round.group(:user).average(:birdies)
+  end
+
+  def pars_rank
+     Round.group(:user).average(:pars)
+  end
+
 end
