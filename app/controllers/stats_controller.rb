4cf728b046c5af577979f77561be50aba6d91ee9
waders92 <wade.carlson@pga.com>
2017-02-09 12:10:25 -0600

    fixed toggle for login status

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,7 @@
 class StatsController < ApplicationController
   def index
     @rounds = Round.order('created_at DESC').limit(15)
-    @messages = Message.limit(15).order('created_at DESC')
+    @messages = Message.limit(20).order('created_at DESC')
     @comments = Comment.all
   end
 
