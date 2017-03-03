fafb808f994134745cdc13bf32ee796286252876
waders92 <wade.carlson@pga.com>
2017-03-03 14:35:18 -0600

    installed fog gem and configured amazons3 accoutn for image hosting

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'figaro', '>= 1.0.0'
 gem 'stripe'
 gem 'carrierwave', '~> 1.0'
 gem 'mini_magick', '~> 4.3.1'
+gem 'fog'
 
 group :development, :test do
   gem 'factory_girl_rails', '~> 4.0'
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,9 +5,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
+  if Rails.env.development?
+    storage :file
+  else
+    storage :aws
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
