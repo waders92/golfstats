e74de3d09c8a9ac133d8ac71ac5d2a5dd0aa11fc
waders92 <wade.carlson@pga.com>
2017-03-03 14:38:36 -0600

    aws to fog

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,7 +8,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   if Rails.env.development?
     storage :file
   else
-    storage :aws
+    storage :fog
   end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
