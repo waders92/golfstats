55a45b9804687d590634cae220aa756300092b14
waders92 <wade.carlson@pga.com>
2017-04-02 15:13:14 -0500

    refactor methods for rankings and leaderboards

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -23,78 +23,6 @@ module RoundsHelper
     Round.group(:user).average(:pars)
   end
 
-  def best_scoring_average_name
-    scoring_rank.sort.each do |(user, avg)|
-      return user.name
-    end
-  end
-
-  def best_scoring_average
-    scoring_rank.sort.each do |(user, avg)|
-      return avg
-    end
-  end
-
-  def best_greens_rank_name
-    greens_rank.sort_by{|k, v| -v}.each do |(user, avg)|
-      return user.name
-    end
-  end
-
-  def best_greens_rank
-    greens_rank.sort_by{|k, v| -v}.each do |(user, avg)|
-      return avg
-    end
-  end
-
-  def best_faiwrways_rank_name
-    fairways_rank.sort_by{|k, v| -v}.each do |(user, avg)|
-      return user.name
-    end
-  end
-
-  def best_faiwrways_rank
-    fairways_rank.sort_by{|k, v| -v}.each do |(user, avg)|
-      return avg
-    end
-  end
-
-  def best_putting_rank_name
-    putting_rank.sort.each do |(user, avg)|
-      return user.name
-    end
-  end
-
-  def best_putting_rank
-    putting_rank.sort.each do |(user, avg)|
-      return avg
-    end
-  end
-
-  def best_birdie_average_name
-    birdies_rank.sort_by{|k, v| -v}.each do |(user, avg)|
-      return user.name
-    end
-  end
-
-  def best_birdie_rank
-    birdies_rank.sort_by{|k, v| -v}.each do |(user, avg)|
-      return avg
-    end
-  end
-
-  def best_par_rank_name
-    pars_rank.sort_by{|k, v| -v}.each do |(user, avg)|
-      return user.name
-    end
-  end
-
-  def best_par_rank
-    pars_rank.sort_by{|k, v| -v}.each do |(user, avg)|
-      return avg
-    end
-  end
-
   def scoring_rank_with_index
     scoring_rank.sort_by{|k, v| +v }.each.with_index(1) do |(user, avg), i|
       yield([user, avg], i)
@@ -103,6 +31,66 @@ module RoundsHelper
 
   def best_scoring_rank_with_index
     scoring_rank_with_index do |(user, avg), i|
+      return [user, avg]
+    end
+  end
+
+  def greens_rank_with_index
+    greens_rank.sort_by{|k, v| -v}.each.with_index(1) do |(user, avg), i|
+      yield([user, avg], i)
+    end
+  end
+
+  def best_greens_rank_with_index
+    greens_rank_with_index do |(user, avg), i|
+      return [user, avg]
+    end
+  end
+
+  def fairways_rank_with_index
+    fairways_rank.sort_by{|k, v| -v}.each.with_index(1) do |(user, avg), i|
+      yield([user, avg], i)
+    end
+  end
+
+  def best_faiwrways_rank_with_index
+    fairways_rank_with_index do |(user, avg), i|
+      return [user, avg]
+    end
+  end
+
+  def putting_rank_with_index
+    putting_rank.sort_by{|k, v| +v }.each.with_index(1) do |(user, avg), i|
+      yield([user, avg], i)
+    end
+  end
+
+  def best_putting_rank_with_index
+    putting_rank_with_index do |(user, avg), i|
+      return [user, avg]
+    end
+  end
+
+  def birdies_rank_with_index
+    birdies_rank.sort_by{|k, v| -v}.each.with_index(1) do |(user, avg), i|
+      yield([user, avg], i)
+    end
+  end
+
+  def best_birdies_rank_with_index
+    birdies_rank_with_index do |(user, avg), i|
+      return [user, avg]
+    end
+  end
+
+  def pars_rank_with_index
+    pars_rank.sort_by{|k, v| -v}.each.with_index(1) do |(user, avg), i|
+      yield([user, avg], i)
+    end
+  end
+
+  def best_pars_rank_with_index
+    pars_rank_with_index do |(user, avg), i|
       return [user, avg]
     end
   end
