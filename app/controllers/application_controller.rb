1fc417685bc392a8d9d88523f3de8840c7e94ab1
waders92 <wade.carlson@pga.com>
2017-04-24 15:36:05 -0500

    custom user path after edit and create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,10 @@ class ApplicationController < ActionController::Base
     response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
   end
 
+  def user_root_path
+    dashboard_path
+  end
+
   protected
 
   def configure_permitted_parameters
