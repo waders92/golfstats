93b1cac0cac94397ec281fa56026be838720268a
waders92 <wade.carlson@pga.com>
2017-04-24 15:11:10 -0500

    fixing ipad font sizing issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || allrounds_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || dashboard_path
   end
 
 
