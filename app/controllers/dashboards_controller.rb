4c99987db29a58b5577b1a331fedbbd994053805
waders92 <wade.carlson@pga.com>
2017-05-23 13:35:13 -0500

    fixing round table disply by when the round was created

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,8 +6,6 @@ class DashboardsController < ApplicationController
   end
 
   def show
-    @rounds = Round.all
-    @monthly_rounds = current_user.rounds.order('created_at DESC').group_by(&:month)
     @yearly_rounds = current_user.rounds.order('created_at DESC').group_by(&:year)
   end
 end
