2c5f134ec8d535f26f24bb1887816a16b78abe70
waders92 <wade.carlson@pga.com>
2017-05-25 12:56:53 -0500

    can render the notes edit page, working on update action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,10 +8,22 @@ class NotesController < ApplicationController
   end
 
   def edit
+    round = Round.find_by_id(params[:round_id])
+    @note = round.notes.find(params[:id])
+  end
 
+  def update
+    round = Round.find_by_id(params[:round_id])
+    @note = round.notes.find(params[:id])
+    @note.update_attributes(note_params.merge(user: current_user))
+    redirect_to round_path(round)
   end
 
   private
+
+  def render_not_found(status = :not_found)
+    render text: status.to_s.titleize.to_s, status: status
+  end
 
   def note_params
     params.require(:note).permit(:message)
