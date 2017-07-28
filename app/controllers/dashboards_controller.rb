4012cb2e4d0eaa26f18c5b870005bf1b05f3de1b
waders92 <wade.carlson@pga.com>
2017-07-28 11:08:27 -0500

    notes updated

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,6 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @rounds = Round.all
-  end
-
   def show
     @monthly_rounds = current_user.rounds.order('created_at DESC').group_by(&:month)
   end
